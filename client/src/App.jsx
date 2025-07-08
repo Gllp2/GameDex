1af2dde93687dcf0d936629c3ab8f8b7c8b9915a
Gonçalo Laureano <167298965+Gllp2@users.noreply.github.com>
2025-07-08 09:43:33 +0100

    frontend first upload

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum vitae, ab esse consectetur exercitationem ad laboriosam iste eum? Recusandae sunt voluptatibus et vitae dolorem nam, magnam ratione ducimus quidem corporis!
+      </div>
+      <div className="Nav">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ratione dolores iusto reprehenderit similique eius velit fugit. Veniam officiis expedita consequuntur tempora quos. Doloremque maiores illum autem ipsam ipsa aliquid?
+      </div>
+    </div>
+  );
+}
+
+export default App;
