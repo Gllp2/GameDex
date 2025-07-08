7f5eff8fce9e1eb5bd9e726991271afc4fcaaf6a
Gonçalo Laureano <167298965+Gllp2@users.noreply.github.com>
2025-07-08 17:34:27 +0100

    SignIn and SignUp Pages Made (mobile and Desktop)

---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import FloatingLogosBackground from '../components/floating-logos-background';
+import SignUp from '../components/signUp';
+
+function SignUpPage() {
+  return (
+    <div>
+        <FloatingLogosBackground />
+        <SignUp />
+    </div>
+  );
+}
+
+export default SignUpPage;
